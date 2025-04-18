31ae696d26c47a7a0afc01bc3b544956596d01a7
Fabian <fbenjumeau@gmail.com>
2025-04-18 18:43:59 -0500

    AGREGADO RUTA PARA ACTUALIZAR LA DESCRIPCION DEL PRODUCTO

--- routes/products.ts
+++ routes/products.ts
@@ -9,4 +9,7 @@ router.get('/', verifyToken, ProductController.getAllProducts);
 router.get('/:id', verifyToken, ProductController.getProductById);
 router.put('/:id', verifyToken, ProductController.updateProduct);
 router.delete('/:id', verifyToken, ProductController.deleteProduct);
+router.patch('/update-descripcion/:id', verifyToken, ProductController.updateDescripcion);
+
+
 export default router;
